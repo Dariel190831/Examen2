2330508b378065c5eafd5a396c880af8428dbd18
darielmencia750 <darielmencia750@gmail.com>
2025-03-26 20:33:04 -0600

    Codigo Completo del examen.

---
+++ ExamenFinalDT/ExamenFinalDT/Models/Tarea.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExamenFinalDT.Models
+{
+    [Table("Tarea")]
+    public class Tarea
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
